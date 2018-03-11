f772d888c91a6ef0b6420430176d1d04c7542b5b
Lluís Revilla <lluis.revilla@gmail.com>
2018-03-11 11:41:06 +0100

    Ensure that the simulated GeneSetCollections has the right properties.

--- R/simulateGSC.R
+++ R/simulateGSC.R
@@ -3,6 +3,7 @@
 #' Simulates a GeneSetCollection following a distribution
 #' @param ppg The number of pathways per genes of the desired GeneSetCollection
 #' @return A GeneSetCollection
+#' @export
 y <- function(ppg) {
   # Check input
   if (any(ppg < 0) || !is.numeric(ppg)) {
@@ -27,26 +28,70 @@ y <- function(ppg) {
     stop("Impossible combination of genes and pathways")
   }
 
-  # For each gene create a random pathway of the size set ppg
-  # Could be done in parallel, as each gene doesn't depend on the previous
-  genes2pathsG <- lapply(ppg, function(y) {
-    lapply(seq_len(y), sample, x = genes)
-  })
+  # Workhorse of the function
+  helper <- function(ppg, genes) {
+    # For each gene create a random pathway of the size set ppg
+    # Could be done in parallel, as each gene doesn't depend on the previous
+    genes2pathsG <- lapply(ppg, function(y) {
+      lapply(seq_len(y), sample, x = genes)
+    })
 
-  # Remove duplicate pathways
-  pathways <- unique(unlist(genes2pathsG, recursive = FALSE,
-                               use.names = FALSE))
-  names(pathways) <- paste0("GS_", seq_along(pathways))
-  genes2pathways <- inverseList(pathways)
-  as(genes2pathways, "GeneSetCollection")
+    # Remove duplicate pathways
+    pathways <- unique(unlist(genes2pathsG, recursive = FALSE,
+                              use.names = FALSE))
+    names(pathways) <- paste0("GS_", seq_along(pathways))
+    genes2pathways <- inverseList(pathways)
+    suppressWarnings(obj <- as(genes2pathways, "GeneSetCollection"))
+    obj
+  }
+
+  gsc <- helper(ppg, genes)
+
+  # Calculate the data
+  ppg_sim <- pathwaysPerGene(gsc)
+  t_ppg_sim <- table(ppg_sim)
+  t_ppg <- table(ppg)
+
+  # Checking the output fits the rules for a pathwayª
+  size <- length(ppg) == length(ppg_sim)
+  tables <- length(t_ppg) == length(t_ppg_sim)
+  if (tables) {
+    repet <- all(names(t_ppg) == names(t_ppg_sim))
+  } else {
+    repet <- FALSE
+  }
+  iter <- 1
+
+  while (size & tables & repet) {
+    gsc <- helper(ppg, genes)
+
+    # Calculate the data
+    ppg_sim <- pathwaysPerGene(gsc)
+    t_ppg_sim <- table(ppg_sim)
+    t_ppg <- table(ppg)
+
+    # Checking the output fits the rules for a pathwayª
+    size <- length(ppg) == length(ppg_sim)
+    tables <- length(t_ppg) == length(t_ppg_sim)
+    if (tables) {
+      repet <- all(names(t_ppg) == names(t_ppg_sim))
+    } else {
+      repet <- FALSE
+    }
+    iter <- iter +1
+  }
+  message("Iterations: ", iter)
+  gsc
 }
 
 
 #' Simulate GeneSetCollection
 #'
-#' Simulates a GeneSetCollection following a distribution
-#' @param gpp The number of pathways per genes of the desired GeneSetCollection
+#' Simulates a GeneSetCollection following the number of pathways per genes
+#' introduced.
+#' @param gpp The number of pathways per genes of the desired GeneSetCollection.
 #' @return A GeneSetCollection
+#' @export
 z <- function(gpp) {
   # Check input
   if (any(gpp < 0) || !is.numeric(gpp)) {
@@ -72,44 +117,138 @@ z <- function(gpp) {
     stop("Impossible combination of genes and pathways")
   }
 
-  # For each pathway create a random group of genes
-  # Could be done in parallel, as each gene doesn't depend on the previous
-  genes2pathsG <- lapply(gpp, function(y) {
-    lapply(seq_len(y), sample, x = pathways)
-  })
 
-  # Remove duplicate pathways
-  genes2pathways <- unique(unlist(genes2pathsG, recursive = FALSE,
-                            use.names = FALSE))
-  names(genes2pathways) <- paste0("G_", seq_along(genes2pathways))
-  as(genes2pathways, "GeneSetCollection")
+  helper <- function(gpp, pathways){
+    # For each pathway create a random group of genes
+    # Could be done in parallel, as each gene doesn't depend on the previous
+    genes2pathsG <- lapply(gpp, function(y) {
+      lapply(seq_len(y), sample, x = pathways)
+    })
+
+    # Remove duplicate pathways
+    genes2pathways <- unique(unlist(genes2pathsG, recursive = FALSE,
+                                    use.names = FALSE))
+    names(genes2pathways) <- paste0("G_", seq_along(genes2pathways))
+
+    suppressWarnings(obj <- as(genes2pathways, "GeneSetCollection"))
+    obj
+  }
+
+  obj <- helper(gpp, pathways)
+
+  # Calculate the data
+  gpp_sim <- genesPerPathway(obj)
+  t_gpp_sim <- table(gpp_sim)
+  t_gpp <- table(gpp)
+
+  # Checking the output fits the rules for a pathwayª
+  size <- length(gpp) == length(gpp_sim)
+  tables <- length(t_gpp) == length(t_gpp_sim)
+  if (tables) {
+    repet <- all(names(t_gpp) == names(t_gpp_sim))
+  } else {
+    repet <- FALSE
+  }
+
+  iter <- 1
+
+  while (size & tables & repet) {
+    obj <- helper(gpp, pathways)
+    iter <- iter +1
+
+    # Calculate the data
+    gpp_sim <- genesPerPathway(obj)
+    t_gpp_sim <- table(gpp_sim)
+    t_gpp <- table(gpp)
+
+    # Checking the output fits the rules for a pathwayª
+    size <- length(gpp) == length(gpp_sim)
+    tables <- length(t_gpp) == length(t_gpp_sim)
+    if (tables) {
+      repet <- all(names(t_gpp) == names(t_gpp_sim))
+    } else {
+      repet <- FALSE
+    }
+  }
+  message("Iterations: ", iter)
+  obj
+
 }
 
+#' Simulate a GeneSetCollection
+#'
+#' Creates a GeneSetCollection with the desired number of genes per pathway
+#' given the number of genes.
+#' @param gpp Genes per pathway.
+#' @param nGenes Number of genes.
+#' @return A GeneSetCollection with those genes distributed in those pathways.
+#' @seealso \code{\link{w}}
+#' @export
+z2 <- function(gpp, nGenes) {
 
-z2 <- function(gpp, ng) {
-
-  stopifnot(ng >= max(gpp))
-  stopifnot(ng > 2)
+  stopifnot(nGenes >= max(gpp))
+  stopifnot(nGenes > 2)
 
   if (any(gpp == 1)) {
     warning("Removing pathways of one gene")
     gpp <- gpp[gpp >= 2]
   }
 
-  genes <- paste0("G_", seq_len(ng))
-  paths2genes <- lapply(gpp, sample, x = genes)
-  as(paths2genes, "GeneSetCollection")
+  genes <- paste0("G_", seq_len(nGenes))
+  if (is.null(names(gpp))) {
+    names(gpp) <- paste0("GS", seq_along(gpp))
+  }
+  helper <- function(gpp, genes){
+    paths2genes <- lapply(gpp, sample, x = genes)
+    suppressWarnings(gsc <- as(inverseList(paths2genes), "GeneSetCollection"))
+    gsc
+  }
+  obj <- helper(gpp, genes)
+  i <- 1
+
+  while(nGenes(gsc) != nGenes) {
+    i <- i +1
+    gsc <- helper(gpp, genes)
+  }
+
+  message("Iterations: ", i)
+  gsc
 }
 
+#' Simulate a GeneSetCollection
+#'
+#' Creates a GeneSetCollection with the desired number of pathways per gene
+#' given the number of pathways.
+#' @param ppg Pathways per gene.
+#' @param nPathways Number of genes.
+#' @return A GeneSetCollection with those genes distributed in those pathways.
+#' @seealso \code{\link{w}}
+#' @export
+w <- function(ppg, nPathways) {
 
-w <- function(ppg, np) {
+  stopifnot(max(ppg) <= nPathways )
+  stopifnot(nPathways > 2)
 
-  stopifnot(max(ppg) <= np )
-  stopifnot(np > 2)
+  if (is.null(names(ppg))) {
+    names(ppg) <- paste0("G_", seq_along(ppg))
+  }
 
-  paths <- paste0("GS_", seq_len(np))
-  genes2paths <- sapply(ppg, sample, x = paths)
-  as(inverseList(genes2paths), "GeneSetCollection")
+  paths <- paste0("GS_", seq_len(nPathways))
+  helper <- function(ppg, paths) {
+    genes2paths <- sapply(ppg, sample, x = paths)
+    suppressWarnings(obj <- as(genes2paths, "GeneSetCollection"))
+    obj
+  }
+
+  gsc <- helper(ppg, paths)
+  iter <- 1
+  while(nPathways(gsc) != nPathways) {
+    iter <- iter + 1
+    gsc <- helper(ppg, paths)
+  }
+
+  message("Iterations: ", iter)
+  obj
 }
 
 
