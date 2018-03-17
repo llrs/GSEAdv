dd9e4ac512f4eea6738b328b18111835cfba2cd1
Lluís Revilla <lluis.revilla@gmail.com>
2018-03-17 13:35:09 +0100

    Add completness function

---
+++ R/completness.R
@@ -0,0 +1,15 @@
+#' Number of possible combinations
+#'
+#' Calculates the number of combinations of each size.
+#' @param x A numeric vector
+#' @return A named numeric vector
+#' @examples
+#' genesPerPathway <- c(2, 4, 5, 8, 5, 8)
+#' completness(genesPerPathway)
+completness <- function(x){
+  iter <- seq_len(max(x))
+  out <- vapply(iter, choose, FUN.VALUE = numeric(1), n = length(x))
+  names(out) <- as.character(iter)
+  out
+}
+
