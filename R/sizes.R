b7ebf62a08a8c8e8e83644da25d24e527c99a677
Lluís Revilla <lluis.revilla@gmail.com>
2018-03-24 12:18:28 +0100

    Add methods to find the relationship between sizes and its elements

--- R/sizes.R
+++ R/sizes.R
@@ -1,52 +1,108 @@
-sizeGenes <- function(object) {
-  object <- check(object)
+#' @describeIn sizeGenes Number of genes per pathway in all genes
+#' @export
+setMethod("sizeGenes",
+          signature(object = "GeneSetCollection", gene = "missing"),
+          function(object) {
+            object <- check(object)
 
-  gpp <- genesPerPathway(object)
-  ppg <- pathwaysPerGene(object)
+            gpp <- genesPerPathway(object)
+            ppg <- pathwaysPerGene(object)
 
-  uPPG <- unique(ppg)
-  uGPP <- unique(gpp)
+            uPPG <- unique(ppg)
+            uGPP <- unique(gpp)
 
-  paths2genes <- GSEABase::geneIds(object)
-  genes2paths <- inverseList(paths2genes)
+            paths2genes <- GSEABase::geneIds(object)
+            genes2paths <- inverseList(paths2genes)
 
-  # Create the matrix to fill
-  m <- matrix(0, ncol = length(ppg), nrow = length(uGPP),
-              dimnames =
-                list("genesPerPathway" = uGPP[order(uGPP)],
-                     "Genes" = names(ppg)))
+            # Create the matrix to fill
+            m <- matrix(0, ncol = length(ppg), nrow = length(uGPP),
+                        dimnames =
+                          list("genesPerPathway" = uGPP[order(uGPP)],
+                               "Genes" = names(ppg)))
 
-  for (gene in colnames(m)) {
-    paths <- genes2paths[[gene]]
-    pSize <- table(gpp[paths])
-    m[names(pSize), gene] <- pSize
-  }
-  m
-}
+            for (gene in colnames(m)) {
+              paths <- genes2paths[[gene]]
+              pSize <- table(gpp[paths])
+              m[names(pSize), gene] <- pSize
+            }
+            m
+          }
+)
+
+#' @describeIn sizePathways Number of pathways per gene in all pathways
+#' @export
+setMethod("sizePathways",
+          signature(object = "GeneSetCollection", pathway = "missing"),
+          function(object) {
+
+            object <- check(object)
+
+            gpp <- genesPerPathway(object)
+            ppg <- pathwaysPerGene(object)
+
+            uPPG <- unique(ppg)
+            uGPP <- unique(gpp)
+
+            paths2genes <- GSEABase::geneIds(object)
+            genes2paths <- inverseList(paths2genes)
+
+            # Create the matrix to fill
+            m <- matrix(0, ncol = length(gpp), nrow = length(uPPG),
+                        dimnames =
+                          list("pathwaysPerGene" = uPPG[order(uPPG)],
+                               "Pathway" = names(gpp)))
+
+            for (path in colnames(m)) {
+              genes <- paths2genes[[path]]
+              pSize <- table(ppg[genes])
+              m[names(pSize), path] <- pSize
+            }
+            m
+          }
+)
 
 
-sizePaths <- function(object) {
-  object <- check(object)
+#' @describeIn sizePathways Number of pathways per gene in all pathways
+#' @export
+setMethod("sizePathways",
+          signature(object = "GeneSetCollection", pathway = "character"),
+          function(object, pathway) {
 
-  gpp <- genesPerPathway(object)
-  ppg <- pathwaysPerGene(object)
+            object <- check(object)
 
-  uPPG <- unique(ppg)
-  uGPP <- unique(gpp)
+            gpp <- genesPerPathway(object)
+            ppg <- pathwaysPerGene(object)
 
-  paths2genes <- GSEABase::geneIds(object)
-  genes2paths <- inverseList(paths2genes)
+            uPPG <- unique(ppg)
+            uGPP <- unique(gpp)
 
-  # Create the matrix to fill
-  m <- matrix(0, ncol = length(gpp), nrow = length(uPPG),
-              dimnames =
-                list("pathwaysPerGene" = uPPG[order(uPPG)],
-                     "Pathway" = names(gpp)))
+            paths2genes <- GSEABase::geneIds(object)
+            genes2paths <- inverseList(paths2genes)
 
-  for (path in colnames(m)) {
-    genes <- paths2genes[[path]]
-    pSize <- table(ppg[genes])
-    m[names(pSize), path] <- pSize
-  }
-  m
-}
+            if (length(pathway) >= 1) {
+              if (any(!pathway %in% names(paths2genes))) {
+                keep <- pathway %in% names(paths2genes)
+                if (sum(keeep) >= 1) {
+                  remove <- pathway[!keep]
+                  pathway <- pathway[keep]
+                  warning("Omitting ", remove, "pathway.\nIt wasn't present")
+                } else {
+                  stop("No provided pathway is present.")
+                }
+              }
+              out <- sapply(pathway, function(x){
+                genes <- paths2genes[[path]]
+                pSize <- table(ppg[genes])
+              })
+            } else {
+              if (pathway %in% names(paths2genes)) {
+                genes <- paths2genes[[path]]
+                out <- table(ppg[genes])
+              } else {
+                stop("No pathway is present.")
+              }
+            }
+
+            out
+          }
+)
