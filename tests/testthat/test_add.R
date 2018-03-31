fd827d0e7b8a55d1b066e48a51ee94fa0d1b9a4a
Lluís Revilla <lluis.revilla@gmail.com>
2018-03-31 17:02:03 +0200

    Correcting an error in the test

--- tests/testthat/test_add.R
+++ tests/testthat/test_add.R
@@ -3,26 +3,27 @@ context("Testing add method")
 expect_warning(Info <- as.GeneSetCollection(info))
 
 test_that("gene as character", {
-  gsc <- add(test, gene = "2", pathway = "1430728")
+  gsc <- add(Info, gene = "2", pathway = "1430728")
 
   expect_true("2" %in% names(inverseList(geneIds(gsc))))
-  expect_equal(nGenes(gsc), nGenes(test))
+  expect_equal(nGenes(gsc), nGenes(Info))
 
-  gsc <- add(test, gene = c("2", "9"), pathway = "1430728")
+  gsc <- add(Info, gene = c("2", "9"), pathway = "1430728")
   expect_true(all(c("2", "9") %in% names(inverseList(geneIds(gsc)))))
-  expect_equal(nGenes(gsc), nGenes(test))
+  expect_equal(nGenes(gsc), nGenes(Info))
 })
 
 test_that("pathway as character", {
-  gsc <- add(test, gene = "2", pathway = "156581")
+  gsc <- add(Info, gene = c("2", "5", "7"), pathway = "156581")
 
-  expect_equal(nPathways(gsc), nPathways(test))
+  expect_equal(nPathways(gsc), nPathways(Info) +1)
+  expect_equal(nGenes(gsc), nGenes(Info) +2)
 
-  expect_warning(gsc <- add(test, gene = "2", pathway = c("156581")),
+  expect_warning(gsc <- add(Info, gene = "2", pathway = c("156581")),
                  "Removing")
-  expect_error(gsc <- add(test, gene = c("2", "3"),
+  expect_error(gsc <- add(Info, gene = c("2", "3"),
                           pathway = c("156581", "211")),
                "Please")
-  gsc <- add(test, gene = c("2", "3"), pathway = "156581")
-  expect_equal(nPathways(gsc), nPathways(test)+1)
+  gsc <- add(Info, gene = c("2", "3"), pathway = "156581")
+  expect_equal(nPathways(gsc), nPathways(Info)+1)
 })
