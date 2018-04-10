f9f51f8879ca3317ad5e589afbbfbb8293df7747
Lluís Revilla <lluis.revilla@gmail.com>
2018-04-11 00:02:02 +0200

    Add test for fromSizePathways

---
+++ tests/testthat/test_fromSizePathways.R
@@ -0,0 +1,17 @@
+context("Testing drop method")
+
+expect_warning(Info <- as.GeneSetCollection(info))
+
+test_that("fromSizePathways", {
+  sP <- sizePathways(Info)
+
+  expect_message(test <- fromSizePathways(sP), "Iterations: ")
+  expect_equal(sP, sizePathways(test))
+})
+
+# test_that("fromSizePathways", {
+#   sP <- sizePathways(genesKegg)
+#
+#   expect_message(test <- fromSizePathways(sP), "Iterations: ")
+#   expect_equal(sP, sizePathways(test))
+# })
