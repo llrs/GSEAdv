ac4d50124817b5cf25436f7fb0f636b9e4d1d95d
Lluís Revilla <lluis.revilla@gmail.com>
2018-04-22 16:31:55 +0200

    Define a function instead of a method for compare

--- R/nested.R
+++ R/nested.R
@@ -19,37 +19,42 @@ setMethod("nested",
 }
 )
 
-#' @describeIn compare Check which pathways are described in both
-#' GeneSetCollections
-#' @export
-setMethod("compare",
-          signature(object1 = "GeneSetCollection", object2 = "GeneSetCollection"),
-          function(object1, object2) {
 
-            nam1 <- deparse(substitute(object1))
-            nam2 <- deparse(substitute(object2))
+#' Pathways equal between two GeneSetCollections
+#'
+#' Check how many pathways are inside another geneSetCollection
+#' @param object1,object2 A GeneSetCollection
+#' @return The names of the pathways that are equivalent.
+#' @export compare
+compare <- function(object1, object2) {
+  obj1 <- is(object1, "GeneSetCollection")
+  obj2 <- is(object2, "GeneSetCollection")
 
-            paths2genes1 <- geneIds(object1)
-            paths2genes2 <- geneIds(object2)
+  nam1 <- deparse(substitute(object1))
+  nam2 <- deparse(substitute(object2))
 
+  if (!obj1 & !obj2) {
+    stop("Both ", nam1, " and ", nam2, " should be GeneSetCollections.")
+  }
 
+  paths2genes1 <- geneIds(object1)
+  paths2genes2 <- geneIds(object2)
 
-            m1_in2 <- outer(paths2genes1, paths2genes2, all_in_vec)
-            m2_in1 <- outer(paths2genes2, paths2genes1, all_in_vec)
-            m <- t(m2_in1) + m1_in2
-            equal <- sum(m == 2)
-            message(equal, " pathways are the same")
-            if (equal >= 1 ){
-              out <- which(m == 2, arr.ind = TRUE)
-              out[, 1] <- rownames(out)
-              out[, 2] <- colnames(m)[as.numeric(out[, 2])]
-              rownames(out) <- seq_len(nrow(out))
-              colnames(out) <- c(nam1, nam2)
-            } else {
-              return(0)
-            }
-            out
-          }
-)
+  m1_in2 <- outer(paths2genes1, paths2genes2, all_in_vec)
+  m2_in1 <- outer(paths2genes2, paths2genes1, all_in_vec)
+  m <- t(m2_in1) + m1_in2
+  equal <- sum(m == 2)
+  message(equal, " pathways are the same")
+  if (equal >= 1 ){
+    out <- which(m == 2, arr.ind = TRUE)
+    out[, 1] <- rownames(out)
+    out[, 2] <- colnames(m)[as.numeric(out[, 2])]
+    rownames(out) <- seq_len(nrow(out))
+    colnames(out) <- c(nam1, nam2)
+  } else {
+    return(0)
+  }
+  out
+}
 
 
