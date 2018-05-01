96d0cfb2783de5b6fd63783bb21bcf4a25ecd3f8
Lluís Revilla <lluis.revilla@gmail.com>
2018-05-01 20:33:33 +0200

    Improve algorithm
    Now it should work, but not tested in large inputs.

---
+++ R/fromSizeGenes_sizePathways.R
@@ -0,0 +1,39 @@
+fromSizeGenes_sizePathways <- function(sizeGenes, sizePathways) {
+  # Check input
+  stopifnot(sum(rowSums(sizeGenes)/as.numeric(rownames(sizeGenes))) == ncol(sizePathways))
+  stopifnot(sum(rowSums(sizePathways)/as.numeric(rownames(sizePathways))) == ncol(sizeGenes))
+  stopifnot(min(as.numeric(rownames(sizeGenes))) >= 2)
+  genes <- colnames(sizeGenes)
+
+  gpp <- colSums(sizePathways)
+  helper <- function(gene, sizeGenes, sizePathways){
+    # message(gene)
+    gpp_gene <- sizeGenes[, gene]
+    paths <- names(gpp)[gpp %in% names(gpp_gene)]
+    paths <- sizePathways[, paths]
+    paths <- paths[as.character(sum(gpp_gene)), ]
+    base::sample(x = names(paths), size = sum(gpp_gene), prob = paths)
+  }
+
+  genes2paths <- lapply(genes, helper,
+                        sizeGenes = sizeGenes, sizePathways = sizePathways)
+  names(genes2paths) <- genes
+
+  gsc <- as(genes2paths, "GeneSetCollection")
+
+  gpp_gsc <- genesPerPathway(gsc)
+  iter <- 1
+  while(any(gpp_gsc != colSums(sizePathways))) {
+    genes2paths <- lapply(genes, helper,
+                          sizeGenes = sizeGenes, sizePathways = sizePathways)
+    names(genes2paths) <- genes
+
+    gsc <- as(genes2paths, "GeneSetCollection")
+
+    gpp_gsc <- genesPerPathway(gsc)
+    iter <- iter + 1
+  }
+
+  message("Iterations: ", iter)
+  gsc
+}
