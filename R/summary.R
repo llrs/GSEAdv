6e2d5408a820c9bf8e84da39716d65aa9429a813
Lluís Revilla <lluis.revilla@gmail.com>
2018-05-01 20:32:46 +0200

    Made easier the output of summary

--- R/summary.R
+++ R/summary.R
@@ -31,23 +31,26 @@ setMethod("summary",
                      maxPPG = max(ppg),
                      pathways = length(gpp)
             )
-            cat("Genes:", out["genes"], "\n")
-            cat("\tGene in more pathways:", out["maxPPG"], "pathways\n")
-            cat("\th-index:", h_gpp, "genes with at least", h_gpp, "pathways.\n")
-            cat("Pathways:", out["pathways"], "\n")
-            cat("\tBiggest pathway:", out["maxGPP"], "genes\n")
-            cat("\th-index:", h_ppg, "pathways with at least", h_ppg, "genes.\n")
+            msg <- paste0("Genes: ", out["genes"],
+                "\n\tGene in more pathways:", out["maxPPG"], "pathways\n",
+            "\th-index:", h_gpp, "genes with at least", h_gpp, "pathways.\n",
+            "Pathways:", out["pathways"], "\n",
+            "\tBiggest pathway:", out["maxGPP"], "genes\n",
+            "\th-index:", h_ppg, "pathways with at least", h_ppg, "genes.\n")
             if (!out["independence"]) {
-              cat("IC(genesPerPathway):", round(out["ICgpp"], 2),
-                  "(", round(out["ICgpp"]/out["maxICgpp"], 2), "of the maximum)\n")
-              cat("IC(pathwaysPerGene)", round(out["ICppg"], 2),
-                  "(", round(out["ICppg"]/out["maxICppg"], 2), "of the maximum)\n")
+              ind <- paste0("IC(genesPerPathway): ", round(out["ICgpp"], 2),
+                  "(", round(out["ICgpp"]/out["maxICgpp"], 2),
+                  "of the maximum)\nIC(pathwaysPerGene) ",
+                  round(out["ICppg"], 2), "(",
+                  round(out["ICppg"]/out["maxICppg"], 2), "of the maximum)\n")
             } else {
-              cat("All genes in a single gene set.\n")
+              ind <- "All genes in a single gene set.\n"
             }
             if (out["isolation"]) {
-              cat("Some gene set have all genes not present in other gene sets")
+              ind <- paste0(ind,
+                            "Some gene set have all genes not present in other gene sets")
             }
+            cat(msg, ind)
             invisible(out)
           }
 )
