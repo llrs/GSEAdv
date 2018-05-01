f8c2ee871167702ac8a8e3aaca945ba29abbe041
Lluís Revilla <lluis.revilla@gmail.com>
2018-05-01 20:30:10 +0200

    Continue the work in the draft

--- R/expand.R
+++ R/expand.R
@@ -6,8 +6,16 @@ expand <- function(obj, genes) {
          Please install it with install.packages('fitdistrplus')")
   }
 
-
   # Find the distribution of genes and pathways
+  ppg <- pathwaysPerGene(obj)
+  gpp <- genesPerPathway(obj)
+
+  #Fit a distribution
+  f <- fitdistrplus::fitdist(ppg, "geom")
+  ppg_sim <- rgeom(length(genes), f$estimate)
+  f <- fitdistrplus::fitdist(gpp, "geom")
+  gpp_sim <- rgeom(length(), f$estimate)
+
   # Follow the distribution/pattern
   # A raw distribution (fitted)
   # The interaction
