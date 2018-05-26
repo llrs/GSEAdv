46e776bace3be86efaa2214a0ae5d8082857690a
Lluís Revilla <lluis.revilla@gmail.com>
2018-05-26 12:38:22 +0200

    Testing compare function

--- tests/testthat/test_nested.R
+++ tests/testthat/test_nested.R
@@ -6,3 +6,14 @@ test_that("nested", {
   expect_true(all(diag(n) == 1L))
   expect_equal(n[1, 2], 1L)
 })
+
+
+test_that("compare", {
+  expect_message(comp <- GSEAdv::compare(Info, genesKegg),
+                 "pathways are the same")
+  expect_equal(comp, 0L)
+  expect_message(comp <- GSEAdv::compare(Info, Info))
+  expect_true(is(comp, "matrix"))
+  expect_equal(ncol(comp), 2L)
+  expect_equal(nrow(comp), 11L)
+})
