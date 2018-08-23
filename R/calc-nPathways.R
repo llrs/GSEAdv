160b2f9e6389638e2440f167b4bf8f68cdadbe63
Lluís Revilla <lluis.revilla@gmail.com>
2018-08-23 17:46:02 +0200

    Functions to estimate the number of genes or pathways given the oposite

---
+++ R/calc-nPathways.R
@@ -0,0 +1,25 @@
+# TODO: Estimates change if we assume that the original object pass the check function
+
+#' Estimate the number of pathways
+#'
+#' Given the number of genes calculates the number of possible pathways
+#' @param nGenes Numeric value of number of genes
+#' @return Numeric value of number of possible pathways
+#' @examples
+#' calc.nPathways(6)
+#' @export
+calc.nPathways <- function(nGenes){
+  sum(vapply(seq_len(nGenes), choose, n = nGenes, numeric(1L)))-1
+}
+
+#' Estimate the number of genes
+#'
+#' Given the number of pathways calculates the minimum number of possible genes
+#' @param nPathways Numeric value of number of pathways
+#' @return Numeric value of number of possible genes
+#' @examples
+#' calc.nGenes(6)
+calc.nGenes <- function(nPathways) {
+  min_nGenes <- nPathways +1
+  min_nGenes
+}
---
+++ tests/testthat/test_calcnPathways.R
@@ -0,0 +1,8 @@
+context("calc.nPathways")
+
+
+
+test_that("works", {
+  expect_equal(calc.nPathways(6L), 62L)
+})
+
