e1323fbd35e8060e79e7a5be67a2e801635197cb
Lluís Revilla <lluis.revilla@gmail.com>
2018-11-25 13:10:01 +0100

    Correct test
    I forgot to remove itself from the counts

--- tests/testthat/test-distr.R
+++ tests/testthat/test-distr.R
@@ -2,12 +2,12 @@ context("test-distr")
 
 test_that("distrGenes works", {
   out <- distrGenes(Info)
-  expect_true(all(colSums(out) == ncol(out)))
+  expect_true(all(colSums(out) == (ncol(out) - 1)))
   expect_equal(ncol(out), nGenes(Info))
 })
 
 test_that("distrPathways works", {
   out <- distrPathways(Info)
-  expect_true(all(colSums(out) == ncol(out)))
+  expect_true(all(colSums(out) == (ncol(out) - 1)))
   expect_equal(ncol(out), nPathways(Info))
 })
