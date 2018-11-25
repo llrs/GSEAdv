cd31deb5f5739543687cd1ed243827a010a99b65
Lluís Revilla <lluis.revilla@gmail.com>
2018-11-25 12:36:49 +0100

    Add tests for distrGenes and distrPathways

---
+++ tests/testthat/test-distr.R
@@ -0,0 +1,13 @@
+context("test-distr")
+
+test_that("distrGenes works", {
+  out <- distrGenes(Info)
+  expect_true(all(colSums(out) == ncol(out)))
+  expect_equal(ncol(out), nGenes(Info))
+})
+
+test_that("distrPathways works", {
+  out <- distrPathways(Info)
+  expect_true(all(colSums(out) == ncol(out)))
+  expect_equal(ncol(out), nPathways(Info))
+})
